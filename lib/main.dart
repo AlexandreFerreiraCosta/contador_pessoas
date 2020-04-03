31690a1991962ea1b9f840cc6233e9aa8cd9b523
Alexandre Ferreira Costa <alexandre.costa@basis,com.br>
2020-04-02 22:35:37 -0300

    feat: Finalizando layout e funcionalidade da aplicação.
    feat: Finalizando layout e funcionalidade da aplicação.

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,95 @@ import 'package:flutter/rendering.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Pessoa : 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "Pode entrar !!",
-            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
-          )
-        ],
-      )));
+      home: Home()));
 }
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _pessoas = 0;
+  String _infoTexto = "Pode entrar !!";
+
+  void _mudarPessoas(int valor){
+    setState(() {
+      _pessoas += valor;
+    });
+
+    if(_pessoas < 0){
+       _infoTexto = "Valor invalido !!";
+       _pessoas = 0;
+       _audioPlayer.play("audio/alert_high-intensity.wav");
+    }else if(_pessoas < 10){
+      _infoTexto = "Pode entrar !!";
+    }else{
+      _infoTexto = "Ambiente lotado!!";
+      _pessoas = 10;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurante.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoa : $_pessoas",
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {_mudarPessoas(1);},
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {_mudarPessoas(-1);},
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              _infoTexto,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+
